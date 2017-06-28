cbeff64367fd0ffcf158d41bf30dca43a0e460e8
Nic Brauer <npbrauer@gmail.com>
2017-06-28 15:50:39 +0000

    first commit - intro-to-programming/basics/basics-exercises.rb

---
+++ intro-to-programming/basics/basics-exercises.rb
@@ -0,0 +1,47 @@
+# Exercise 1
+
+first_name = "Nic"
+last_name = "Brauer"
+full_name = first_name + " " + last_name
+puts full_name
+
+# Exercise 2
+
+puts "Here is a 4 digit number: 1958"
+num = 1958
+thou_place = num / 1000
+hund_place = num / 100 % 10
+tens_place = num / 10 % 10
+ones_place = num % 10
+puts "Thousands place: " + thou_place.to_s
+puts "Hundreds place: " + hund_place.to_s
+puts "Tens place: " + tens_place.to_s
+puts "Ones place: " + ones_place.to_s
+
+# Exercise 3
+
+fam_bdays = {:tim => '1958', :sue => '1960', :nic => '1990', :jenna => '1996', :kendra => '1989'}
+puts "Tim was born in " + fam_bdays[:tim]
+puts "Sue was born in " + fam_bdays[:sue]
+puts "Nic was born in " + fam_bdays[:nic]
+puts "Jenna was born in " + fam_bdays[:jenna]
+puts "Kendra was born in " + fam_bdays[:kendra]
+
+# Exercise 4
+
+fam_bdays_array = ['1958', '1960', '1990', '1996', '1989']
+puts "Tim was born in " + fam_bdays_array[0]
+puts "Sue was born in " + fam_bdays_array[1]
+puts "Nic was born in " + fam_bdays_array[2]
+puts "Jenna was born in " + fam_bdays_array[3]
+puts "Kendra was born in " + fam_bdays_array[4]
+
+# Exercise 5
+
+
+
+# Exercise 6
+
+
+
+# Exercise 7
